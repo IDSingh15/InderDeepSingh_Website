982021ad11e2cb3d6f5723b229f4ab50e5f98018
Inder Deep Singh <idsingh15@gmail.com>
2018-01-19 05:37:43 +0530

    Test 4

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
-// JavaScript Document
+// main.js
+
+//To disable right click on the contents
+
+$(document).ready(function () {
+    
+    $('body').bind('cut copy paste', function (e) {
+        e.preventDefault();
+    });
+	$("body").on("contextmenu",function(e){
+        return false;
+    });
+});
+
+//For animation on page 0
+
+$(document).ready(function(){
+	$(".logoimage, .logotext, .menu, .rightindicators, .rights").css("opacity", "0");
+    $(".homeimg").css({"opacity":"0","right":"-10%"});
+	$(".hometext").css({"opacity":"0", "top":"30%"});
+});
+
+$(document).ready(function(){
+	$(".logoimage, .logotext, .menu, .rightindicators, .rights").animate({opacity: '1'}, "slow");
+	$("#page0").css("display", "initial");
+    $("#page0 > .homeimg").animate({opacity:'1', right:'0px'}, "slow");
+	$("#page0 > .hometext").animate({opacity:'1', top:'40%'}, "slow");
+});
+
+//For animation on Page 1
 
